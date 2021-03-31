ba70396b0e4f36808c60e9298dbc4c918e35e9ed
Nicolini <braum.exe@gmail.com>
2021-03-31 02:33:49 -0300

    Node Editor Panel Updates

--- src/main/java/com/valhalla/application/gui/NodeEditor.java
+++ src/main/java/com/valhalla/application/gui/NodeEditor.java
@@ -1,14 +1,53 @@
 package com.valhalla.application.gui;
 
 import javax.swing.*;
+import javax.swing.event.MouseInputListener;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 
-public class NodeEditor extends JPanel {
+class ColorPair {
+    private Class<?> key;
+    private Color color;
 
-    private ArrayList<NodeData> dataTypes;
+    public ColorPair(Class<?> dataClass, Color color) {
+        Constructor<?> constructor = dataClass.getConstructors()[0];
+        try {
+            Object data = constructor.newInstance();
+            if(!(data instanceof NodeData)) {
+                throw new NullPointerException();
+            }
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new NullPointerException();
+        }
 
+        this.key = dataClass;
+        this.color = color;
+    }
 
-    public NodeEditor() {}
+    NodeData GetNodeData() {
+        Constructor<?> constructor = key.getConstructors()[0];
+        try {
+            Object data = constructor.newInstance();
+            if(!(data instanceof NodeData)) {
+                throw new NullPointerException();
+            }
+            return (NodeData) data;
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new NullPointerException();
+        }
+    }
+}
+
+public class NodeEditor extends JPanel implements MouseWheelListener, MouseMotionListener, MouseInputListener {
 
     /*
         -- Components
@@ -18,8 +57,124 @@ public class NodeEditor extends JPanel {
             - EditorBar -> Multiple tools (compile, simulate, etc)
      */
 
+    private ArrayList<NodeData> dataTypes;
+    private ArrayList<ColorPair> dataColors;
+
+    double zoom = 1.0;
+    private int spacing;
+    private Point origin = new Point(0,0);
+    private Point mousePt;
+    private boolean wheelPressed;
+
+    public NodeEditor() {
+        this.setLayout(new GridLayout(1,1));
+        this.addMouseWheelListener(this);
+        this.addMouseMotionListener(this);
+        this.addMouseListener(this);
+        this.setOpaque(true);
+        this.wheelPressed = false;
+        //this.setBackground(Color.GREEN);
+    }
+
+    public NodeEditor(Component c) {
+        this();
+        c.addMouseWheelListener(this);
+        //this.setBackground(Color.GREEN);
+        add(c);
+    }
+
     void RegisterDataTypes(NodeData[] dataTypes) {
+        this.dataTypes.addAll(Arrays.asList(dataTypes));
+
+        for (NodeData nData : dataTypes) {
+            Random rand = new Random();
+            float r = rand.nextFloat();
+            float g = rand.nextFloat();
+            float b = rand.nextFloat();
+            System.out.println(nData.getClass());
+            dataColors.add(new ColorPair(nData.getClass(), new Color(r, g, b)));
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D graphics = (Graphics2D) g;
+        graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        graphics.drawLine(0, origin.y, getWidth(), origin.y);
+        graphics.drawLine(origin.x, 0, origin.x, getHeight());
+        // set up grid
+        int x = 0;
+        int y = 0;
+        graphics.setColor(new Color(220, 220, 220));
+        while (x < getWidth()) {
+            graphics.drawLine(origin.x + x, 0, origin.x + x, getHeight());
+            x += 20;
+        }
+        while (y < getHeight()) {
+            graphics.drawLine(0, origin.y + y, getWidth(), origin.y + y);
+            y += 20;
+        }
+    }
+
+    public void paintChildren(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.scale(zoom, zoom);
+        super.paintChildren(g2);
+    }
+
+    @Override
+    public void mouseWheelMoved(MouseWheelEvent e) {
+        //int ticks = e.getWheelRotation();
+        //zoom *= Math.pow(1.2, ticks);
+
+        //System.out.println("xd");
+
+        repaint();
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
 
     }
 
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        if(wheelPressed)
+            System.out.println("MEEEP");
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        if(e.getButton() == MouseEvent.BUTTON2) {
+            wheelPressed = true;
+
+            origin.x += e.getPoint().x;
+            origin.y += e.getPoint().y;
+
+            repaint();
+        }
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        if(e.getButton() == MouseEvent.BUTTON2) {
+            wheelPressed = false;
+        }
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }
--- src/main/java/com/valhalla/application/gui/ProjectSelectorFrame.java
+++ src/main/java/com/valhalla/application/gui/ProjectSelectorFrame.java
@@ -42,7 +42,8 @@ public class ProjectSelectorFrame
         //======== this ========
         setTitle("Quester");
         Container contentPane = getContentPane();
-        contentPane.setLayout(new BorderLayout());
+        //contentPane.setLayout(new BorderLayout());
+        contentPane.setLayout(new MigLayout("", "[grow]", "[grow]"));
 
         {
             //======== fileMenu ========
@@ -103,63 +104,66 @@ public class ProjectSelectorFrame
         //appHeaderPanel.add(logoImage);
         //this.setPreferredSize(logoImage.getPreferredSize());
 
-        JPanel panel = new JPanel();
-        //BoxLayout lyt = new BoxLayout(panel, BoxLayout.Y_AXIS);
-        MigLayout lyt = new MigLayout("", "[grow][20][grow]", "[grow]");
-        panel.setLayout(lyt);
-        JPanel headerPanel = new JPanel(new MigLayout("","[shrink]", "[grow]"));
+        //JPanel panel = new JPanel();
+        ////BoxLayout lyt = new BoxLayout(panel, BoxLayout.Y_AXIS);
+        //MigLayout lyt = new MigLayout("", "[grow][20][grow]", "[grow]");
+        //panel.setLayout(lyt);
+        //JPanel headerPanel = new JPanel(new MigLayout("","[shrink]", "[grow]"));
+//
+        //headerPanel.setBackground(new Color(0,0,0,30));
+        //headerPanel.setBorder(BorderFactory.createEmptyBorder(5, 5,5,0));
+//
+        //ImagePanel img = new ImagePanel("img/dribbbleLogo.png");
+        //img.setAlignmentX(Component.LEFT_ALIGNMENT);
+        //img.setAlignmentY(Component.TOP_ALIGNMENT);
+        //headerPanel.add(img, "west, shrink");
+//
+        //JLabel titleLabel = new JLabel("WELCOME TO QUESTER");
+        //titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        //titleLabel.setAlignmentY(Component.BOTTOM_ALIGNMENT);
+        //titleLabel.setHorizontalAlignment(SwingConstants.TRAILING);
+        //titleLabel.setBorder(BorderFactory.createEmptyBorder(0, 10,0,0));
+        //Font ibmPlex = new Utils().getFontFromResource("fonts/Rubik-Bold.ttf");
+        //if(ibmPlex != null) {
+        //    titleLabel.setFont(ibmPlex.deriveFont(14f));
+        //}
+        //JLabel subtitleLabel = new JLabel("Select or Create Project");
+        //subtitleLabel.setBorder(BorderFactory.createEmptyBorder(0, 10,0,0));
+//
+        //headerPanel.add(titleLabel, "growx, wrap");
+        //headerPanel.add(subtitleLabel);
+        //panel.add(headerPanel, "dock north, shrink 0, wrap 1");
+//
+        //JPanel panelButtons = new JPanel();
+        //panelButtons.setLayout(new BoxLayout(panelButtons, BoxLayout.Y_AXIS));
+        //panelButtons.setBorder(new EmptyBorder(0,0,0,0));
+//
+        //SelectorButton btn = new SelectorButton("New Project", Utils.Icon.plus);
+        //SelectorButton btn2 = new SelectorButton("Open Project", Utils.Icon.folder);
+        //SelectorButton btn3 = new SelectorButton("Settings", Utils.Icon.settings);
+//
+        //btn.AddSelectorClickListener(() -> {
+        //    NewProjectFrame newProject = new NewProjectFrame();
+        //    //final JDialog frame = new JDialog(newProject, "New Project", true);
+        //    //newProject.getContentPane().add(panel);
+        //    this.setVisible(false);
+        //    newProject.pack();
+        //    newProject.setVisible(true);
+        //});
+//
+        //panelButtons.add(btn);
+        //panelButtons.add(btn2);
+        //panelButtons.add(btn3);
+//
+        //panel.add(new JLabel(""));
+        //panel.add(panelButtons, "w 200!, gaptop 20");
+        //NodeProperty[] props = {new ImageProperty()};
+        //panel.add(new ImageNode());
+        //panel.add(new JTextPane());
 
-        headerPanel.setBackground(new Color(0,0,0,30));
-        headerPanel.setBorder(BorderFactory.createEmptyBorder(5, 5,5,0));
+        //contentPane.add(panel, BorderLayout.NORTH);
 
-        ImagePanel img = new ImagePanel("img/dribbbleLogo.png");
-        img.setAlignmentX(Component.LEFT_ALIGNMENT);
-        img.setAlignmentY(Component.TOP_ALIGNMENT);
-        headerPanel.add(img, "west, shrink");
 
-        JLabel titleLabel = new JLabel("WELCOME TO QUESTER");
-        titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        titleLabel.setAlignmentY(Component.BOTTOM_ALIGNMENT);
-        titleLabel.setHorizontalAlignment(SwingConstants.TRAILING);
-        titleLabel.setBorder(BorderFactory.createEmptyBorder(0, 10,0,0));
-        Font ibmPlex = new Utils().getFontFromResource("fonts/Rubik-Bold.ttf");
-        if(ibmPlex != null) {
-            titleLabel.setFont(ibmPlex.deriveFont(14f));
-        }
-        JLabel subtitleLabel = new JLabel("Select or Create Project");
-        subtitleLabel.setBorder(BorderFactory.createEmptyBorder(0, 10,0,0));
-
-        headerPanel.add(titleLabel, "growx, wrap");
-        headerPanel.add(subtitleLabel);
-        panel.add(headerPanel, "dock north, shrink 0, wrap 1");
-
-        JPanel panelButtons = new JPanel();
-        panelButtons.setLayout(new BoxLayout(panelButtons, BoxLayout.Y_AXIS));
-        panelButtons.setBorder(new EmptyBorder(0,0,0,0));
-
-        SelectorButton btn = new SelectorButton("New Project", Utils.Icon.plus);
-        SelectorButton btn2 = new SelectorButton("Open Project", Utils.Icon.folder);
-        SelectorButton btn3 = new SelectorButton("Settings", Utils.Icon.settings);
-
-        btn.AddSelectorClickListener(() -> {
-            NewProjectFrame newProject = new NewProjectFrame();
-            //final JDialog frame = new JDialog(newProject, "New Project", true);
-            //newProject.getContentPane().add(panel);
-            this.setVisible(false);
-            newProject.pack();
-            newProject.setVisible(true);
-        });
-
-        panelButtons.add(btn);
-        panelButtons.add(btn2);
-        panelButtons.add(btn3);
-
-        panel.add(new JLabel(""));
-        panel.add(panelButtons, "w 200!, gaptop 20");
-        NodeProperty[] props = {new ImageProperty()};
-        panel.add(new ImageNode());
-        panel.add(new JTextPane());
-
-        contentPane.add(panel, BorderLayout.NORTH);
+        contentPane.add(new NodeEditor(), "grow");
     }
 }
